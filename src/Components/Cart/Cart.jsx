d4a299da2f2c7b77303a21d34a724967bfbb9210
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-03-30 21:29:27 +0600

    added calculation

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -3,10 +3,25 @@ import './Cart.css';
 
 const Cart = (props) => {
     const {cart} = props;
+
+    let totalShipping = 0;
+    let totalPrice = 0;
+    for(const product of cart){
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+
+    const tax = totalPrice*3/100;
+
+    const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
-            <h2>Order Summary</h2>
+            <h2 style={{fontSize: '21px'}}>Order Summary</h2>
             <p>Selected Items: {cart.length}</p>
+            <p>Total price: $ {totalPrice.toFixed(2)} </p>
+            <p>Shipping:$ {totalShipping.toFixed(2)}</p>
+            <p>Tax(3%): $ {tax.toFixed(2)}</p>
+            <h3 style={{fontSize:'16px'}}>Grand Total:$ {grandTotal.toFixed(2)} </h3>
         </div>
     );
 };
