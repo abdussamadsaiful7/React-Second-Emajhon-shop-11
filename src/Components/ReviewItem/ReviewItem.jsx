f87daabaad14e3cfbd2ddb1dbe0d3ff92417f8a3
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-04-06 01:15:22 +0600

    added set delete handler

--- src/Components/ReviewItem/ReviewItem.jsx
+++ src/Components/ReviewItem/ReviewItem.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTrashAlt} from '@fortawesome/free-solid-svg-icons'
 
-const ReviewItem = ({product}) => {
+const ReviewItem = ({product, handleRemoveFromCart}) => {
+    const {id, img, price, quantity, name} = product;
     return (
-        <div>
-            <h2>Review Item</h2>
+        <div className='review-item'>
+            <img className='image' src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>$ {price}</span></p>
+                <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
+            </div>
+            <button onClick={()=>handleRemoveFromCart(id)} className='button'> 
+            <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} /> 
+            </button>
         </div>
     );
 };
